ffffe4efc701b9372dfdd6257448fc39148f8ff2
Rohit Howlader <rohithowlader2017@gmail.com>
2022-08-31 23:37:41 +0530

    Database Connection Succesfull

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
+import connectDB from './config/db.mjs';
 
-
+connectDB();
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -10,7 +11,7 @@ app.get('/',(req,res)=>{
 });
 
 //Server Setup
-const PORT= process.env.PORT || 3000;
+const PORT= process.env.PORT || 5000;
 app.listen(PORT, () =>{
     console.log(`App is running on port : ${PORT}`);
 })
